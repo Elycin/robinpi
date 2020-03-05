61d56acce39d7e9833cb57a14a97e0f952c716e7
Ely Haughie <ely@elyc.in>
2020-03-05 00:14:35 -0800

    Implementing seven segment port,

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from Robinhood import Robinhood
 import configparser
 import time
 
+from luma.led_matrix.device import max7219
+from luma.core.interface.serial import spi, noop
+from luma.core.virtual import viewport, sevensegment
+
 config = configparser.ConfigParser()
 config.read('config.ini')
 
@@ -12,7 +16,13 @@ robinhood_interface.login(
     qr_code=config['robinhood']['multi_factor_secret']
 )
 
+# create seven segment device
+serial = spi(port=0, device=0, gpio=int(config['GPIO']['DIN']))
+device = max7219(serial, cascaded=1)
+seg = sevensegment(device)
+
 while True:
     equity = round(float(robinhood_interface.portfolios()['equity']), 2)
     print(equity)
+    seg.text = str(equity)
     time.sleep(float(config['ticker']['refresh_rate']))

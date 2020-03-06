3a11d18152b2bab86f0b63aff093878133d97e83
Ely Haughie <elycin2@gmail.com>
2020-03-06 07:45:23 -0800

    Exception display appears redundant.

--- main.py
+++ main.py
@@ -18,20 +18,16 @@ serial = spi(port=0, device=0, gpio=noop())
 device = max7219(serial)
 seg = sevensegment(device)
 
-try:
-    robinhood_interface = Robinhood()
-    robinhood_interface.login(
-        username=config['robinhood']['username'],
-        password=config['robinhood']['password'],
-        mfa_code=config['robinhood']['multi_factor_secret']
-    )
+robinhood_interface = Robinhood()
+robinhood_interface.login(
+    username=config['robinhood']['username'],
+    password=config['robinhood']['password'],
+    mfa_code=config['robinhood']['multi_factor_secret']
+)
 
-    while True:
-        equity = round(float(robinhood_interface.portfolios()['extended_hours_portfolio_equity']), 2)
-        print(equity)
-        seg.text = str(equity)
-        time.sleep(float(config['ticker']['refresh_rate']))
-except Exception as e:
-    seg.text = "API ERR"
-    raise e
+while True:
+    equity = round(float(robinhood_interface.portfolios()['extended_hours_portfolio_equity']), 2)
+    print(equity)
+    seg.text = str(equity)
+    time.sleep(float(config['ticker']['refresh_rate']))
 
